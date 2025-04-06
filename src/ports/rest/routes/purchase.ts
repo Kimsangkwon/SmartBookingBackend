b733abd4742709b0be6d23e70580d73f8a2032f1
Kimsangkwon <118443780+Kimsangkwon@users.noreply.github.com>
2025-04-05 23:26:15 -0400

    Login user can get the email when purchasing

--- src/ports/rest/routes/purchase.ts
+++ src/ports/rest/routes/purchase.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { Request, Response } from "express";
 import { authenticateToken } from "../middleware/authentication";
 import { createPurchase } from "../../../infrastructure/mongodb/queries/purchase";
+import { transporter } from "../../../config/email";
 
 const router = Router();
 
@@ -18,7 +19,26 @@ router.post("/", authenticateToken, async (req: Request, res: Response) => {
       //calculate total amount
       const totalAmount = quantity * price;
       const newPurchase = await createPurchase({userId,billingCardId,eventId,eventName,eventDate,eventImage,eventVenue,quantity,price,totalAmount});
-  
+      
+      await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: (req as any).user.email,
+        subject: "Your SmartBooking Ticket Confirmation",
+        text: `
+        Thank you for your purchase!
+        
+Event: ${eventName}
+Venue: ${eventVenue}
+Date: ${eventDate}
+Price per Ticket: ${price}
+Tickets: ${quantity}
+Total: $${totalAmount.toFixed(2)}
+
+Your ticket ID: ${newPurchase._id.toString().slice(-8).toUpperCase()}
+
+Please keep this email as proof of your purchase.
+      `
+              })
       res.status(201).json({ message: "Purchase successful", purchase: newPurchase });
     } catch (error) {
       console.error("Error creating purchase:", error);
